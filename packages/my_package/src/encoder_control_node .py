916dccd731d6ecd06273b2c5f0316f4f2ec1def9
MariMachaidze <marimachaidze30@gmail.com>
2025-04-16 19:54:03 +0200

    before trial changes

--- packages/my_package/src/encoder_control_node .py
+++ packages/my_package/src/encoder_control_node .py
@@ -9,40 +9,37 @@ from duckietown_msgs.msg import WheelEncoderStamped
 
 class EncoderControlNode(DTROS):
     def __init__(self, node_name):
-        super(EncoderControlNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+        super(EncoderControlNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
         
-        vehicle_name = os.environ['VEHICLE_NAME']
+        self.vehicle_name = os.environ['VEHICLE_NAME']
         wheels_topic = f"/{vehicle_name}/wheels_driver_node/wheels_cmd"
         self._publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
 
-
-    def __init__(self, node_name):
-        # initialize the DTROS parent class
-        super(WheelEncoderReaderNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
         # static parameters
-        self._vehicle_name = os.environ['VEHICLE_NAME']
         self._left_encoder_topic = f"/{self._vehicle_name}/left_wheel_encoder_node/tick"
         self._right_encoder_topic = f"/{self._vehicle_name}/right_wheel_encoder_node/tick"
+
         # temporary data storage
         self._ticks_left = None
         self._ticks_right = None
+
         # construct subscriber
         self.sub_left = rospy.Subscriber(self._left_encoder_topic, WheelEncoderStamped, self.callback_left)
         self.sub_right = rospy.Subscriber(self._right_encoder_topic, WheelEncoderStamped, self.callback_right)
 
+    def callback_left(self, data):
+        # log general information once at the beginning
+        rospy.loginfo_once(f"Left encoder resolution: {data.resolution}")
+        rospy.loginfo_once(f"Left encoder type: {data.type}")
+        # store data value
+        self._ticks_left = data.data
 
-
-
-
-
-
-
-
-
-
-
-
-
+    def callback_right(self, data):
+        # log general information once at the beginning
+        rospy.loginfo_once(f"Right encoder resolution: {data.resolution}")
+        rospy.loginfo_once(f"Right encoder type: {data.type}")
+        # store data value
+        self._ticks_right = data.data
         
     def set_speed(self, left_speed, right_speed, duration):
         """Sets wheel speed for a specific duration."""
@@ -54,6 +51,21 @@ class EncoderControlNode(DTROS):
         rate.sleep()
 
     def run(self):
+
+        '''
+
+        # publish received tick messages every 0.05 second (20 Hz)
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            if self._ticks_right is not None and self._ticks_left is not None:
+                # start printing values when received from both encoders
+                msg = f"Wheel encoder ticks [LEFT, RIGHT]: {self._ticks_left}, {self._ticks_right}"
+                rospy.loginfo(msg)
+            rate.sleep()
+            
+            
+        '''
+
         rospy.loginfo("Starting speed sequence...")
          
         max_speed = 0.7
@@ -86,6 +98,11 @@ class EncoderControlNode(DTROS):
             self.set_speed(speed, speed, 0.1)
             # rate = rospy.Rate(10)  # 10 Hz publishing
 
+            if self._ticks_left is not None and self._ticks_right is not None:
+                rospy.loginfo(f"[ENCODERS] Left: {self._ticks_left}, Right: {self._ticks_right}")
+            else:
+                rospy.loginfo("[ENCODERS] Waiting for encoder data...")
+
         print("SPEED", speed)
 
         # 30% speed for 5 seconds
@@ -99,48 +116,3 @@ if __name__ == '__main__':
     node = EncoderControlNode(node_name='encoder_control_node')
     node.run()
 
-
-
-'''
-
-
-
-
-class WheelEncoderReaderNode(DTROS):
-
-
-    def callback_left(self, data):
-        # log general information once at the beginning
-        rospy.loginfo_once(f"Left encoder resolution: {data.resolution}")
-        rospy.loginfo_once(f"Left encoder type: {data.type}")
-        # store data value
-        self._ticks_left = data.data
-
-    def callback_right(self, data):
-        # log general information once at the beginning
-        rospy.loginfo_once(f"Right encoder resolution: {data.resolution}")
-        rospy.loginfo_once(f"Right encoder type: {data.type}")
-        # store data value
-        self._ticks_right = data.data
-
-    def run(self):
-        # publish received tick messages every 0.05 second (20 Hz)
-        rate = rospy.Rate(20)
-        while not rospy.is_shutdown():
-            if self._ticks_right is not None and self._ticks_left is not None:
-                # start printing values when received from both encoders
-                msg = f"Wheel encoder ticks [LEFT, RIGHT]: {self._ticks_left}, {self._ticks_right}"
-                rospy.loginfo(msg)
-            rate.sleep()
-
-if __name__ == '__main__':
-    # create the node
-    node = WheelEncoderReaderNode(node_name='wheel_encoder_reader_node')
-    # run the timer in node
-    node.run()
-    # keep spinning
-    rospy.spin()
-
-
-
-'''
